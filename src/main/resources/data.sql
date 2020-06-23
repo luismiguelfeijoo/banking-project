7d9e8a32f4bb5781e08c461f639e60d7075b1fc1
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-06-23 17:11:32 +0200

    Add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO secured_user(id, name, username, password) VALUES
+    (1, 'luismi', 'admin', '$2a$10$hiRq4b7h.wMd3iBMU8fVAObQi9emuudsWvUc/AnP8SAD4iOcJpWFS');
+
+INSERT INTO admin(id) VALUES
+    (1);
+
+INSERT INTO role(id, role, user_id) VALUES
+    (1, 'ROLE_ADMIN', 1),
+    (2, 'ROLE_ACCOUNTHOLDER', 1);
+
+
