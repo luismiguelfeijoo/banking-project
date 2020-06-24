3cf6807f289f4e1f2bf4e9fc3f6fae698ecaebb6
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-06-24 21:52:20 +0200

    add method to admin intergace

--- src/main/java/com/ironhack/midterm/controller/interfaces/AdminController.java
+++ src/main/java/com/ironhack/midterm/controller/interfaces/AdminController.java
@@ -6,10 +6,14 @@ import com.ironhack.midterm.model.Account;
 import com.ironhack.midterm.model.AccountHolder;
 import com.ironhack.midterm.model.ThirdParty;
 
+import java.util.List;
+
 public interface AdminController {
     public Account createAccount(AccountDTO account);
     public AccountHolder createAccountHolder(AccountHolder accountHolder);
     public ThirdParty addThirdParty(ThirdPartyDTO thirdPartyDTO);
+    public List<AccountHolder> getAccountHolders();
+    public AccountHolder getAccountHolder(Long id);
     // Add a view model to show the balance
     public Account accessAccount();
     public void debitAccount();
